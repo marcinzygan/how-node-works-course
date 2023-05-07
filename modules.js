440182fdb7313d864edc9d6f8ae446a60bfa3704
marcinzygan <soul@o2.pl>
2023-05-07 15:40:31 +0200

    node modules lesson

--- modules.js
+++ modules.js
@@ -1,8 +1,13 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
+//module.eports
 const Calculator = require("./test-module-1");
-
 const calc1 = new Calculator();
-
 console.log(calc1.add(2, 5));
+
+//exports
+// const  calc2 = require("./test-module-2");
+const { add, multiply } = require("./test-module-2");
+// console.log(calc2);
+console.log(multiply(5, 8));
